0bae653e8c0f666477a95de67d0af6679e221632
jpandaconnor <connor@jpanda.org>
2017-07-07 22:14:09 +0100

    Initial commit. Base stuff in here

---
+++ bootstrap/app.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+
+    ]
+]);
+
+$app->get('/', function($request, $response) {
+    return 'Home';
+})
+
+?>
